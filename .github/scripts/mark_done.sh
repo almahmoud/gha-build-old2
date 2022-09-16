3bb9585c09d44c24f594d43705747956ebeb1f7f
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-09-16 12:36:49 -0400

    Manual restart

--- .github/scripts/mark_done.sh
+++ .github/scripts/mark_done.sh
@@ -11,4 +11,4 @@ sed -i "s/    \"$PKGTOMARK\": \[\],\?//g" packages.json
 sed -i "s/        \"$PKGTOMARK\",\?//g" packages.json
 sed -i -z 's/,\n\n\+}/}/g' packages.json
 sed -i -z 's/,\n\n\+ *]/]/g' packages.json
-python -c 'import json; f = open("packages.json", "r"); pkgs = json.load(f); f.close(); f = open("packages.json", "w"); f.write(json.dumps(pkgs, indent=4)); f.close()'
+python3 -c 'import json; f = open("packages.json", "r"); pkgs = json.load(f); f.close(); f = open("packages.json", "w"); f.write(json.dumps(pkgs, indent=4)); f.close()'
--- .github/scripts/rclone_push.sh
+++ .github/scripts/rclone_push.sh
@@ -12,7 +12,7 @@ sed -i "s/    \"$PKGTOMARK\": \[\],\?//g" packages.json
 sed -i "s/        \"$PKGTOMARK\",\?//g" packages.json
 sed -i -z 's/,\n\n\+}/}/g' packages.json
 sed -i -z 's/,\n\n\+ *]/]/g' packages.json
-python -c 'import json; f = open("packages.json", "r"); pkgs = json.load(f); f.close(); f = open("packages.json", "w"); f.write(json.dumps(pkgs, indent=4)); f.close()'
+python3 -c 'import json; f = open("packages.json", "r"); pkgs = json.load(f); f.close(); f = open("packages.json", "w"); f.write(json.dumps(pkgs, indent=4)); f.close()'
 git add lists
 git add packages.json
 git commit -m "Mark pushed $PKGTOMARK"
