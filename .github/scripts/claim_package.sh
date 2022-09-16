b06289a5b4a99ebff0944d456c13afc5cc746a37
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-09-16 13:29:04 -0400

    Fix claim again

--- .github/scripts/claim_package.sh
+++ .github/scripts/claim_package.sh
@@ -5,7 +5,7 @@ git pull origin main || git reset --hard origin/main
 mkdir -p lists
 touch tobuild.txt
 if [ ! -s tobuild.txt ]; then
-      grep -Pzo "(?s)\s*\"\N*\":\s*\[\s*\]" packages.json | awk -F'"' '{print $2}' | grep -v '^$' | xargs -i bash -c 'touch lists/{} && if ! [ -s "lists/failed/{}" ]; then if ! grep -i ".tar.gz$" lists/{}; then if [ $(cat lists/{} | wc -l) = "1" ]; then echo "readytobuild" > lists/{}; fi; fi; fi'
+      grep -Pzo "(?s)\s*\"\N*\":\s*\[\s*\]" packages.json | awk -F'"' '{print $2}' | grep -v '^$' | xargs -i bash -c 'touch lists/{} && if ! [ -s "lists/failed/{}" ]; then if ! grep -i ".tar.gz$" lists/{}; then echo "readytobuild" > lists/{}; fi; fi'
 
       grep -lr "readytobuild" lists/ | sed 's#lists/##g' > tobuild.txt
       git add lists
---
+++ .github/scripts/populate_readme.py
@@ -0,0 +1,41 @@
+import json
+from os.path import exists
+from tabulate import tabulate
+with open("original.json", "r") as f:
+    pkgs = json.load(f)
+import requests
+
+table = []
+
+for pkg in list(pkgs):
+    name = pkg
+    runid = ""
+    runurl = ""
+    status = "Unclaimed"
+    tarname = ""
+    plog = ""
+    if exists(f"logs/run_ids/{pkg}"):
+        with open(f"logs/run_ids/{pkg}", "r") as frun:
+            runid = frun.read()
+    if "https://github.com/" in runid:
+        runurl = runid.strip().replace("null\n", "")
+        r = requests.get(runurl)
+        if r.status_code == 404:
+            runurl = runurl.replace("gha-build", "gha-build-old")
+            r = requests.get(runurl)
+        if r.status_code == 200:
+            name = f"[{pkg}]({runid.strip()})"
+    if exists(f"lists/failed/{pkg}"):
+        status = "Failed"
+    elif exists(f"lists/{pkg}"):
+        with open(f"lists/{pkg}", "r") as pf:
+            plog = pf.read()
+    if plog.endswith("tar.gz\n"):
+        status = "Succeeded"
+        tarname = plog.strip()
+    table.append([name, status, tarname])
+
+
+headers = ["Package", "Status", "Tarball"]
+with open("README.md", "w") as f:
+    f.write(tabulate(table, headers, tablefmt="github"))
